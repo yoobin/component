2dc2c1a51d16445d0b908d21c293fecdc03944a6
yoobin <yoobin@ttadag.com>
2016-06-30 20:08:59 +0900

    달력 버전2 완성이지만 리펙토리 반드시 필수!

---
+++ index_v1.js
@@ -0,0 +1,140 @@
+/**
+ * @description
+ * 현재까지의 버전 2016.6.25 18:08
+ * 추후 개선 사항, v2에서 추후 기능을 구현..
+ * 0. 달력을 일치하기 랜더 O
+ * 1. getMonth + 1문제 해결 1빼도 정상적인 숫자가 나오게.x
+ * 2. 달력 태이블 상단에 <> 넣어 년도와 월을 이동x
+ * 3. 해당 날짜를 선택할 경우 얼랏으로 현재 date 표시x
+ * 4. 주말 구분 추가x
+ * 5. 달력 template보다 강도 높게...x
+ */
+
+var component = {};
+
+(function(){
+
+	'use strict';
+
+	/**
+	 * @description
+	 * 1. index.html에 위치에 상관없이 #caledner를 생성하여 사용
+	 */
+
+	component.DEFINETEMPLATE = {
+		header : '',
+		body : [
+			'<tabel>',
+				'<thead>',
+					'<tr><th>Sun</th><th>Mon</th><th>Tues</th><th>Wednes</th><th>Thurs</th><th>Fri</th><th>Satur</th></tr>',
+				'</thead>',
+				'<tboday>',
+					'<tr class="week">',
+						'<td></td>',
+						'<td></td>',
+						'<td></td>',
+						'<td></td>',
+						'<td></td>',
+						'<td></td>',
+						'<td></td>',
+					'</tr>',
+				'</tbody>',
+			'</table>'
+		].join(''),
+		footer : ''
+	};
+
+	component.TEMPLATE = {
+		body :
+			'<table>' +
+			'<thead>' +
+				'<tr>' +
+			'       <th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th>' +
+			'   </tr>' +
+			'</thead>' +
+			'<tbody>'
+	};
+
+	/**
+	 * <button>전년도</button><button>이전달</button><h1>6월</h1><button>다음달</button><button>다음해</button>
+	 * <table>
+	 *     <thead>
+	 *         <tr>
+	 *             <th></th>
+	 *             <th></th>
+	 *         </tr>
+	 *     </thead>
+	 *     <tbody>
+	 *         <tr>
+	 *             <td></td>
+	 *             <td></td>
+	 *         </tr>
+	 *         </tbody>
+	 * </table>
+	 */
+
+
+
+
+
+
+
+	component.Calendar = function(options) {
+
+		this.options = options;
+		var self = this,
+			currentDate = new Date(),
+			currentLastDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, ''),
+			weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+		//현재 년도
+		this.options.getCurrentYear = currentDate.getFullYear();
+		//현재 월
+		this.options.getCurrentMonth = currentDate.getMonth() + 1;
+		//현재 일
+		this.options.getCurrentTodate = currentDate.getDate();
+		//현재 월 마지막 일
+		this.options.getCurrentLastDate = currentLastDate.getDate();
+		//현재 달의 시작 요일
+		this.options.getFirstDay = weeks[(new Date(currentDate.getFullYear(), currentDate.getMonth(), 1)).getDay()];
+		//현재 달의 시작 위치 일
+		this.options.getFirstStartDay = (new Date(currentDate.getFullYear(), currentDate.getMonth(), 1)).getDay();
+		//현재 달의 마지막 요일
+		this.options.getLastDay = weeks[(new Date(currentDate.getFullYear(), currentDate.getMonth(), currentLastDate.getDate()).getDay())];
+		//월의 row개수...
+		this.options.getWeekRow = Math.ceil(((new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay())+currentLastDate.getDate())/7);
+console.log(this.options);
+		(function() {
+
+			var calendarEl = document.getElementById(self.options.id),
+				tempEl = "<h1>" +self.options.getCurrentMonth + "</h1>" + component.TEMPLATE.body,
+				tempNum = 1,
+				tempNum2 = 1;
+
+			for(var i = 1; i <= self.options.getWeekRow; i++){
+				tempEl += "<tr>";
+				for(var k = 1; k <= 7; k++) {
+
+					tempEl += "<td>";
+					if((tempNum <= self.options.getFirstStartDay) || (tempNum2 > self.options.getCurrentLastDate)) {
+						tempEl += "";
+					} else {
+						tempEl += tempNum2;
+						tempNum2++;
+					}
+					tempNum++;
+
+
+					tempEl += "</td>"
+				}
+				tempEl += "</tr>";
+			}
+
+			tempEl +="</tbody></table>";
+			calendarEl.innerHTML = tempEl;
+		})();
+
+		return this;
+	};
+
+})();
---
+++ index_v2.js
@@ -0,0 +1,307 @@
+
+'use strict';
+
+/**
+ * calendar v2 mvc...
+ *  createElement, innerHTML, appendChild...
+ *  createElement = dom형태의 태그를 생성함
+ *  appendChild = dom형태 태그를 append
+ *  innerHTML = string html tag 존재하는경우 dom으로 변환하여 붙임
+ *  innerText = string html tag 존재하더라도 dom으로 변환없이 붙임
+ *
+ */
+var sui = {};
+sui.component = {};
+var Model = function(attributes) {
+
+	this.attributes = attributes || {};
+
+	if (attributes.collection) {
+		this.collection = attributes.collection;
+	}
+};
+
+var Collection = function(models) {
+	this.models = [];
+	this.add(models);
+};
+
+var eventBind = function(context) {
+	var self = this;
+
+	return function() {
+		return self.apply(context);
+	};
+};
+
+var View = function(attributes) {
+	var self = this;
+	this.model = attributes.model;
+	this.tagName = attributes.tagName || 'div';
+
+	for (var properties in attributes) {
+		if ( !this.hasOwnProperty(properties) ) {
+			this[properties] = attributes[properties];
+		}
+
+	}
+
+	this.el = document.createElement(this.tagName);
+
+
+	if (attributes.className) {
+		this.el.setAttribute('class', attributes.className);
+	}
+
+	if (attributes.template) {
+		this.el.innerHTML = attributes.template;
+	}
+
+	if (attributes.events) {
+		var delegateEventSplitter = /^(\S+)\s*(.*)$/;
+		///./g,''         : 특정문자 제거2 (,)
+
+		//이벤트 등록하는 부분인데 따로 함수를 만들어서 빼야할듯... 보다 정교하게 개선해야함...
+		for (var key in attributes.events) {
+			var matched = key.match(delegateEventSplitter);
+
+			var elClassName = this.el.getElementsByClassName(matched[2].replace('.',''));
+			for (var i = 0; i < elClassName.length; i++) {
+
+				var method = attributes[attributes.events[matched[0]]];
+				elClassName[i].addEventListener(matched[1], eventBind.apply(method, [this]));
+
+			}
+		}
+	}
+
+};
+
+Model.prototype = {
+	set : function(attributes, value) {
+		this.attributes[attributes] = value;
+	},
+	get : function(attributes) {
+		return this.attributes[attributes];
+	}
+};
+
+Collection.prototype = {
+	add : function(models) {
+		for(var i = 0; i < models.length; i++) {
+			//console.log(models[i]);
+			this.models.push(models[i]);
+		}
+	}
+};
+
+View.prototype = {
+
+	//클로저
+	//Array prototype bind apply
+	//Function prototype bind apply
+	//Array prototype slice call
+
+	_bind : function(func, context) {
+
+	}
+
+};
+
+
+
+sui.component.createCalendar = function(options) {
+
+	this.el = document.getElementById(options.elementId);
+
+	var date = new Date();
+
+
+
+	var weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+	var model = new Model({
+				options : options,
+				year : date.getFullYear(),
+				month : date.getMonth() + 1,
+				today : date.getDate()
+
+	});
+	model.set('lastDate', (new Date(model.get('year'), model.get('month'), '')).getDate());
+	model.set('weekRow', Math.ceil((model.get('lastDate') + date.getDay()) / 7));
+	model.set('startDay', weeks[(new Date(model.get('year'), date.getMonth(), 1)).getDay()]);
+	model.set('startDate', (new Date(model.get('year'), date.getMonth(), 1)).getDay());
+	model.set('lastDay', weeks[(new Date(date.getFullYear(), date.getMonth(), model.get('lastDate'))).getDay()]);
+
+	//console.log(model.attributes);
+
+
+
+
+	var view = new View({
+		model : model,
+		tagName : 'div',
+		className : 'container',
+		template : '<button class="pre_year">전년도</button><button class="pre_month">이전달</button><h1 class="current_day">' + model.get('year') + '년' + model.get('month') +'월</h1><button class="next_month">다음달</button><button class="next_year">다음해</button>',
+		tableTemplate :
+						'<thead>' +
+						'</thead>' +
+						'<tbody>' +
+						'</tbody>',
+		events : {
+			'click .pre_year' : 'preYear',
+			'click .next_year' : 'nextYear',
+			'click .next_month' : 'nextMonth',
+			'click .pre_month' : 'preMonth'
+		},
+		preYear : function(e) {
+			this.model.set('year', this.model.get('year') - 1 );
+			this.calculation();
+		},
+		nextYear : function(e) {
+			this.model.set('year', this.model.get('year') + 1 );
+			this.calculation();
+		},
+		nextMonth : function() {
+
+			if ( this.model.get('month') < 12 ) {
+				this.model.set('month', this.model.get('month') + 1 );
+			} else {
+				this.model.set('month', 1 );
+			}
+			this.calculation();
+		},
+		preMonth : function() {
+
+			if ( this.model.get('month') > 1) {
+				this.model.set('month', this.model.get('month') - 1 );
+			} else {
+				this.model.set('month', 12 );
+			}
+			this.calculation();
+		},
+		dayRender : function(e) {
+
+			for ( var i = 0; i < this.el.childNodes.length; i++ ) {
+				if ( this.el.childNodes[i].getAttribute('class') === 'current_day') {
+					this.el.childNodes[i].innerText = model.get('year') + '년' + model.get('month') + '월';
+				}
+			}
+
+
+			////날짜 관련 랜더....나머진 내일 와서 랜더링 해보자..
+			if ( !document.getElementsByTagName('table').length ) {
+				var elTable = document.createElement('table');
+
+				elTable.innerHTML = this.tableTemplate;
+
+				for (var k = 0; k < elTable.childNodes.length; k++) {
+
+
+					if (elTable.childNodes[k].tagName.toLowerCase() === 'thead') {
+
+						var elTr = document.createElement('tr');
+
+						for (var w = 0; w < weeks.length; w++) {
+							var elTh = document.createElement('th');
+							elTh.innerText = weeks[w];
+							elTr.appendChild(elTh);
+						}
+						elTable.childNodes[k].appendChild(elTr);
+
+					} else if (elTable.childNodes[k].tagName.toLowerCase() === 'tbody') {
+						this.dateTimeRender(elTable.childNodes[k]);
+						//이곳에서 1~30일까지의 기간을 넣음
+						//console.log(elTable.childNodes[k]);
+						//console.log(this.model.get('weekRow'));
+					}
+
+				}
+
+				this.el.appendChild(elTable);
+			} else {
+				this.dateTimeRender()
+			}
+
+		},
+		dateTimeRender : function(elTbody) {
+
+			if ( !elTbody ) {
+				var elTbody = document.getElementsByTagName('tbody');
+				elTbody = elTbody[0];
+			}
+
+
+
+
+
+
+			if ( elTbody.children.length ) {
+				elTbody.remove();
+				elTbody = document.createElement('tbody');
+				var elTable = document.getElementsByTagName('table');
+
+				elTable[0].appendChild(elTbody);
+
+			}
+
+			var startDateCount = 0;
+			var dateCount = 1;
+
+			for ( var i = 0; i < this.model.get('weekRow'); i++) {
+				var elTr = document.createElement('tr');
+				for ( var k = 0; k < 7; k++ ) {
+					if ( (startDateCount < this.model.get('startDate')) || (dateCount > this.model.get('lastDate')) ) {
+						var elTd = document.createElement('td');
+						elTd.innerText = '';
+						elTr.appendChild(elTd);
+						elTbody.appendChild(elTr);
+					} else {
+						var elTd = document.createElement('td');
+						elTd.innerText = dateCount;
+						elTr.appendChild(elTd);
+						elTbody.appendChild(elTr);
+						dateCount++;
+					}
+					startDateCount++;
+
+				}
+			}
+
+
+		},
+		calculation : function() {
+
+			model.set('lastDate', (new Date(model.get('year'), model.get('month'), '')).getDate());
+			model.set('weekRow', Math.ceil( (model.get('lastDate') + (new Date(model.get('year'), model.get('month') - 1, 1)).getDay() ) / 7));
+			model.set('startDate', (new Date(model.get('year'), model.get('month') - 1, 1)).getDay());
+			model.set('startDay', weeks[(new Date(model.get('year'), model.get('month') - 1, 1)).getDay()]);
+			model.set('lastDay', weeks[(new Date(date.getFullYear(), model.get('month') - 1, model.get('lastDate'))).getDay()]);
+
+			//lastDay 한달의 마지막 요일
+			//weekRow 한달의 주 횟수
+			//startDay 한달의 시작 요일
+			//lastDate 한달의 마지막 일수
+
+			//달력에 뿌릴때 빈숫자 넣는거
+			//console.log(model.get('startDate'));
+			//달력에 본문 데이터 숫자 넣기1~lastDate..
+			//console.log(model.get('lastDate'));
+
+
+
+			this.dayRender();
+
+
+
+
+
+
+		}
+	});
+	view.dayRender();
+
+
+
+
+	this.el.appendChild(view.el);
+};
